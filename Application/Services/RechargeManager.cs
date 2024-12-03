c91dfdda3484fac6ecbc1856604dbbd84723a5b2
steve chisale <Steve.Chisale@nbs.mw>
2024-12-03 10:59:24 +0200

    implemented the application layer

---
+++ Application/Services/RechargeManager.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+
+namespace Application.Services
+{
+    public class RechargeManager : IRechargeService
+    {
+        private readonly Infrastructure.Services.RechargeService _rechargeService;
+
+        public RechargeManager(Infrastructure.Services.RechargeService rechargeService)
+        {
+            _rechargeService = rechargeService;
+        }
+
+        public async Task<RechargeResponse> PerformRechargeAsync(RechargeRequest request)
+        {
+            // Validate the request parameters
+            if (request.Amount <= 0)
+            {
+                return new RechargeResponse
+                {
+                   // Status = "Failed",
+                    //Message = "Invalid recharge amount"
+                };
+            }
+
+            // Call PerformRechargeAsync from the RechargeService and return the result
+            return await _rechargeService.PerformRechargeAsync(request);
+        }
+    }
+}
