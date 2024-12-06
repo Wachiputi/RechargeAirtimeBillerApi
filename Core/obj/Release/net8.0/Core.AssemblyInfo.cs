19988b97a7afac6fcbe9df346f1b038d9a8c6c64
steve chisale <stcchisale03@gmail.com>
2024-12-06 09:54:02 +0200

    added core assembly

--- Core/obj/Release/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Release/net8.0/Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d92b61eeb3185dc2b2cdad006a5e0e85210a03d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
